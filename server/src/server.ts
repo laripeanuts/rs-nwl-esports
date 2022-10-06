ba1c9e849bc36df90aeaea199ea51b7f53d842f0
laripeanuts <larissarabelolf@gmail.com>
2022-10-06 17:56:58 -0300

    stage 3 - server - add database

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,19 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
 
-app.get('/ads', (request, response) => {
+app.get('/games', (request, response) => {
+  return response.json([]);
+});
+
+app.post('/games', (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (request, response) => {
+  const gameId = request.params.id;
+
   return response.json([
     { id: 1, name: "Asd 1 "},
     { id: 2, name: "Asd 2 "},
@@ -10,6 +21,12 @@ app.get('/ads', (request, response) => {
     { id: 4, name: "Asd 4 "},
     { id: 5, name: "Asd 5 "},
   ]);
+});
+
+app.get('/ads/:id/discord', (request, response) => {
+  const adsId = request.params.id;
+
+  return response.json([]);
 });
 
 app.listen(3333);
